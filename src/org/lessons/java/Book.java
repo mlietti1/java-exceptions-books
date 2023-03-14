622db7b1ea52c8a16b4c04656c5290b5a66ed296
mlietti1 <mcf.lietti@outlook.com>
2023-03-14 16:06:10 +0100

    init

---
+++ src/org/lessons/java/Book.java
@@ -0,0 +1,48 @@
+package org.lessons.java;
+
+public class Book {
+
+    private String title;
+    private int pages;
+    private String author;
+    private String publisher;
+
+    public Book(String title, int pages, String author, String publisher) {
+        this.title = title;
+        this.pages = pages;
+        this.author = author;
+        this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
